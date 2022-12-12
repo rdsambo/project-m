72baaed77c0b6c97a5ced6e8799d823d2ee0190d
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-12-12 09:02:49 +0200

    change host url

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -2,7 +2,7 @@ export default function checkEnvironment(): string {
   const envUrl =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : 'https://trello-clone-one.vercel.app';
+      : 'https://trello-git-develop-ineconsole-gmailcom.vercel.app';
 
   return envUrl;
 }
