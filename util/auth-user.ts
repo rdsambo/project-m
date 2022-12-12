53dded28186d283ef57fcc3300389c2356fcf1b9
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-12-12 14:56:19 +0200

    add auth-user

---
+++ util/auth-user.ts
@@ -0,0 +1,21 @@
+import Cookies from 'js-cookie';
+import checkEnvironment from '@/util/check-environment';
+
+class User {
+  constructor(public email: string, public id: string) {}
+}
+
+const getCookieUser = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const email = urlParams.get('email');
+  const id = urlParams.get('id');
+  if (email != null) {
+    Cookies.set('email', email);
+    Cookies.set('id', email);
+    return new User(email, id);
+  }
+  const user = new User(Cookies.get('email'), Cookies.get('id'));
+  return user;
+};
+
+export default getCookieUser;
