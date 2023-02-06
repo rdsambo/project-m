176a6b51b93dd5f49b4143a7d7bafd5afb507517
Agostinhodossantos <agostinhodossantos27@gmail.com>
2023-02-06 11:30:40 +0200

    update NODE_ENV

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -1,8 +1,8 @@
 export default function checkEnvironment(): string {
   const envUrl =
     process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'http://vmi840200.contaboserver.net:8080';
+      ? 'http://vmi840200.contaboserver.net:3000'
+      : 'http://vmi840200.contaboserver.net:3000';
 
   //'https://trello-sepia-gamma.vercel.app
 
