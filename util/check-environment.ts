cba7a21d393220d61fedae30576e3ae3575c38eb
Agostinhodossantos <agostinhodossantos27@gmail.com>
2023-02-06 10:11:39 +0200

    set new envUrl

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -1,8 +1,10 @@
 export default function checkEnvironment(): string {
   const envUrl =
     process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://trello-sepia-gamma.vercel.app';
+      ? 'http://154.12.227.44:3000'
+      : 'http://154.12.227.44:3000';
+
+  //'https://trello-sepia-gamma.vercel.app
 
   return envUrl;
 }
